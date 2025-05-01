c264c64591e5f5a2705a4033d5b506401b3701d5
sugikazu75 <sugihara@jsk.imi.i.u-tokyo.ac.jp>
2025-05-02 06:51:53 +0900

    install clang-format and pre-commit

--- make_init.sh
+++ make_init.sh
@@ -1,5 +1,6 @@
 sudo apt update
-sudo apt install -y ca-certificates curl emacs gnupg htop lsb-release net-tools python3-pip python3-setuptools tmux xclip
+sudo apt install -y ca-certificates clang-format curl emacs gnupg htop lsb-release net-tools python3-pip python3-setuptools tmux xclip
+pip install pre-commit
 
 # gh https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
